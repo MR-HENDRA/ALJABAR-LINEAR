2a4119426d631f507f27eafd890e43dbf6900af4
MR-HENDRA <hunterblackshadow26@gmail.com>
2022-11-01 17:30:00 +0800

    tugas aljabar linear

--- tugas2.py
+++ tugas2.py
@@ -1,19 +1,9 @@
 #HENDRA USMAN (D0221079)
 
 #wadah matriks 3x3
-b = [[0,0,0],
-     [0,0,0],
-     [0,0,0]]
-
-#user input
-for i in range(len(b)):
-    for j in range(len(b)):
-        b[i][j] = int(input(f"Masukkan Nilai Untuk Matriks Posisi {i+1}, {j+1}: "))
-
-#output matriks 3x3
-print("Matriks: ")
-for h in b:
-    print(h)
+b = [[1,2,2],
+     [-2,1,0],
+     [7,-9,3]]
     
 #kalkulasi determinan matriks 3x3
 x = ((b[0][0]*b[1][1]*b[2][2]) + (b[0][1]*b[1][2]*b[2][0]) + (b[0][2]*b[1][0]*b[2][1]))
